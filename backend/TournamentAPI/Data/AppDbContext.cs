1d1ea06025122944a7efbb81f3a9c930566232d7
origin <76100046+wjqm333@users.noreply.github.com>
2026-05-18 13:19:25 +0200

    Backend version med Player API

---
+++ backend/TournamentAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TournamentAPI.Models;
+
+namespace TournamentAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
